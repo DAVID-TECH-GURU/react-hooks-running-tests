904bcf3121cbcc4fd39f426c331aef560821b2d6
DAVID-TECH-GURU <davidmaina823123@gmail.com>
2023-04-22 18:24:39 +0300

    Done with assignment

--- src/components/App.js
+++ src/components/App.js
@@ -9,5 +9,14 @@ function App() {
     <Article />
   </div>;
 }
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Header from "../components/Header";
+
+test("displays the text 'hello from the Header!'", () => {
+  render(<Header />);
+  expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
+});
+
 
 export default App;
